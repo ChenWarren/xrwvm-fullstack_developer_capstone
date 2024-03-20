79db72e7d79a9d29cfd9ed8885c1a47f5a4b0d2d
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2024-03-19 22:51:27 -0700

    added models and views

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,15 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    founded = models.DateField(blank=True, null=True)  # Year the company was founded
+    headquarters = models.CharField(max_length=255, blank=True, null=True)  # Location of headquarters
+    website = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +32,13 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='car_models')
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=20, choices=(('Sedan', 'Sedan'), ('SUV', 'SUV'), ('Hatchback', 'Hatchback'), ('Coupe', 'Coupe'), ('Wagon', 'Wagon')))
+    year = models.IntegerField()
+    engine_size = models.IntegerField(blank=True, null=True)  # in cubic centimeters
+    fuel_type = models.CharField(max_length=10, blank=True, null=True, choices=(('Gasoline', 'Gasoline'), ('Diesel', 'Diesel'), ('Electric', 'Electric'), ('Hybrid', 'Hybrid')))
+
+    def __str__(self):
+        return '{} - {}'.format(self.make.name, self.name)
